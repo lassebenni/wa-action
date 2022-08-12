9e7029dd3476f80576961d0321574fb67837854d
Lasse Benninga <devops.pipeline@example.com>
2022-08-13 00:12:32 +0200

    Fix

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import os
 from twilio.rest import Client
 
 message = os.getenv("INPUT_MESSAGE")
-sender = os.getenv('INPUT_FROM')
-receiver = os.getenv('INPUT_TO')
-account_sid=os.getenv('INPUT_TWILIO_ACCOUNT_SID')
-authtoken=os.getenv('INPUT_TWILIO_AUTH')
+sender = os.getenv("INPUT_FROM")
+receiver = os.getenv("INPUT_TO")
+account_sid = os.getenv("INPUT_TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("INPUT_TWILIO_AUTH")
 
 
-#creating twilio client
+# creating twilio client
 
-sender='whatsapp:'+sender
-receiver='whatsapp:'+receiver
-client=Client()
-client.messages.create( from_=sender, body=message, to=receiver)
+sender = "whatsapp:" + sender
+receiver = "whatsapp:" + receiver
+
+client = Client(account_sid, auth_token)
+message = client.messages.create(from_=sender, body=message, to=receiver)
